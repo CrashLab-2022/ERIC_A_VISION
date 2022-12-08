3d7562b5835669e461123c3c19578321a4d37d91
coldbell8918 <pjc0525@hanyang.ac.kr>
2022-12-08 16:56:12 +0900

    FIX Freq

--- darknet_ros/src/human_detect.py
+++ darknet_ros/src/human_detect.py
@@ -15,25 +15,28 @@ class human():
         self.cnt=0
         self.subs.append(rospy.Subscriber('darknet_ros/bounding_boxes', BoundingBoxes, self.callback))
         self.is_callback=True
+        self.is_service=False
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.demo_callback)
+        
 
     def callback(self, data):
         n=len(data.bounding_boxes)
         for i in range(0,n):
             # rospy.loginfo("hi")
             if data.bounding_boxes[i].id==0:
-                rospy.loginfo(self.cnt)
+                self.is_service=True
 
-                if self.cnt==0:
-                    rospy.loginfo(self.speakclient())
-                    # if self.speakclient() == 'finish':
-                    #     self.cnt=-2
+                # rospy.loginfo(self.cnt)
 
-                self.cnt+=1
+                # if self.cnt==0:
+                #     rospy.loginfo(self.speakclient())
+                #     # if self.speakclient() == 'finish':
+                #     #     self.cnt=-2
 
-                if self.cnt==30:
-                    self.cnt=0
+                # self.cnt+=1
 
-        
+                # if self.cnt==30:
+                #     self.cnt=0
     
     def speakclient(self):
         # rospy.wait_for_service('play_song')
@@ -44,6 +47,13 @@ class human():
 
         except rospy.ServiceException as e:
             rospy.loginfo("Service call failed: ")
+
+    def demo_callback(self, timer):
+        rospy.loginfo("hi")
+        if self.is_service:
+            self.speakclient()
+            self.is_service=False
+
 
 if __name__ == '__main__':
     rospy.init_node('human_detect', anonymous=True)
